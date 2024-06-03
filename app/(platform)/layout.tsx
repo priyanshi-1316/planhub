5040a01d5a96671610bc8d0c47fd8d6c3cfdd6f7
priyanshi <priyanshi12.goel@gmail.com>
2024-06-03 15:35:47 +0530

    autherization

---
+++ app/(platform)/layout.tsx
@@ -0,0 +1,16 @@
+import { ClerkProvider } from "@clerk/nextjs";
+
+
+const Platformlayout=({
+    children
+}: {
+    children: React.ReactNode;
+})=>{
+    return (
+    <ClerkProvider>
+        {children}
+    </ClerkProvider>
+    );
+};
+
+export default Platformlayout;
